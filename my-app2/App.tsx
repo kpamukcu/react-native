84aa6c2d92bfd69ba034ecc56b5a72624bf7269e
kpamukcu <kapawebtasarim@gmail.com>
2025-11-28 12:27:39 +0300

    update

--- my-app2/App.tsx
+++ my-app2/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#007bff',
     alignItems: 'center',
     justifyContent: 'center',
   },
