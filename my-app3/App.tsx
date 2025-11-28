85d593f55a4174a7747098eb5ec07f0a9a9825d7
kpamukcu <kapawebtasarim@gmail.com>
2025-11-29 02:24:56 +0300

    add gif & mp4

--- my-app3/App.tsx
+++ my-app3/App.tsx
@@ -3,36 +3,42 @@ import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-n
 
 export default function App() {
 
-const showInfo = () => {
-  Alert.alert(
-    'Bilgi',                          //Başlığı Oluşturur
-    'Bu Bir Bilglendirme Kutusudur',  //Mesajı Oluşturur
-    [
-      {text:'İptal', onPress:() => console.log('İptal Tıklandı')},      //Alert içindeki birinci butonu oluşturur
-      {text:'Devam', onPress:() => console.log('Devam Et Tıklandı')}    //Alert içindeki ikinci butonu oluşturur
-    ]
-  )
-}
+  const showInfo = () => {
+    Alert.alert(
+      'Bilgi',                          //Başlığı Oluşturur
+      'Bu Bir Bilglendirme Kutusudur',  //Mesajı Oluşturur
+      [
+        { text: 'İptal', onPress: () => console.log('İptal Tıklandı') },      //Alert içindeki birinci butonu oluşturur
+        { text: 'Devam', onPress: () => console.log('Devam Et Tıklandı') }    //Alert içindeki ikinci butonu oluşturur
+      ]
+    )
+  }
 
-const info = () => {
-  Alert.alert(
-    'TouchableOpacity Buton',
-    'Touchable Buton Açıldı',
-    [
-      {text:'Cancel', onPress:() => console.log('Cancel Tıklandı')},
-      {text:'Ok', onPress:() => console.log('Ok Tıklandı')}
-    ]
-  )
-}
+  const info = () => {
+    Alert.alert(
+      'TouchableOpacity Buton',
+      'Touchable Buton Açıldı',
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancel Tıklandı') },
+        { text: 'Ok', onPress: () => console.log('Ok Tıklandı') }
+      ]
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:20}}>Kaan Pamukcu ile <Text style={{fontWeight:900}}>React Native</Text> Dersleri</Text>
-      <Button title='Normal Buton' onPress={showInfo}/>
+      <Text style={{ fontSize: 20, textAlign:'center', marginBottom:25}}>
+        Kaan Pamukcu ile <Text style={{ fontWeight: 900 }}>React Native</Text> Dersleri
+        <Text> Alert Api ile Bildirim Gösterme</Text>
+      </Text>
+
+      <Button title='Normal Buton' onPress={showInfo} />
+
       <TouchableOpacity style={styles.btn2} onPress={info}>
         <Text>Touchable Buton</Text>
       </TouchableOpacity>
-      <StatusBar style="auto"/>
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -44,8 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  btn2:{
-    backgroundColor:'orange',
-    padding:15
+  btn2: {
+    backgroundColor: 'orange',
+    padding: 15
   }
 });
