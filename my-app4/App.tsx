cf38c99e9022aef1c6bb7717f5fc3d3a551bbb78
kpamukcu <kapawebtasarim@gmail.com>
2025-12-01 17:01:29 +0300

    my-app4

--- my-app4/App.tsx
+++ my-app4/App.tsx
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
 
-      {Array.from({ length: 50 }).map((_, i) => (
-        <View key={i}>
+      {Array.from({ length: 10 }).map((_, i) => (
+        <View key={i} style={styles.item}>
           <Text>My-App4 {i + 1} </Text>
         </View>
       ))}
+
+
+      <Image source={require('./assets/web.webp')} />
+      <Image source={require('./assets/web.webp')} />
+      <Image source={require('./assets/web.webp')} />
+
 
       <StatusBar style="auto" />
     </ScrollView>
@@ -22,4 +28,11 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
     alignItems: 'center'
   },
+  item: {
+    width: '90%',
+    padding: 10,
+    backgroundColor: '#a8d2ffff',
+    margin: 5,
+    borderRadius: 10
+  }
 });
