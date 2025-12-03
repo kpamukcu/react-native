d3e2f9df023c1494408191c08dde1e56f91fa392
kpamukcu <kapawebtasarim@gmail.com>
2025-12-03 15:32:09 +0300

    my-app5

--- my-app5/App.tsx
+++ my-app5/App.tsx
@@ -4,34 +4,54 @@ import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 export default function App() {
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <>
+      <ScrollView contentContainerStyle={styles.container} horizontal showsHorizontalScrollIndicator={false}>
+        {Array.from({ length: 30 }).map((_, i) => (
+          <View key={i} style={styles.box}>
+            <Text style={styles.letter}>My-App5 {i + 1} </Text>
+          </View>
+        ))}
+        <StatusBar style="auto" />
+      </ScrollView>
+      <View>
+        <Text style={{ textAlign: 'center', fontSize: 24 }}>Yatay Kaydırma</Text>
+      </View>
+      <ScrollView style={styles.container} horizontal>
+        <Image source={require('./assets/web.webp')} style={styles.gap} />
+        <Image source={require('./assets/grafik.webp')} style={styles.gap} />
+        <Image source={require('./assets/dijital.webp')} style={styles.gap} />
+      </ScrollView>
+      <ScrollView style={styles.container} horizontal>
+        <Image source={require('./assets/web.webp')} style={styles.gap} />
+        <Image source={require('./assets/grafik.webp')} style={styles.gap} />
+        <Image source={require('./assets/dijital.webp')} style={styles.gap} />
+      </ScrollView>
+    </>
 
-      {Array.from({ length: 10 }).map((_, i) => (
-        <View key={i} style={styles.item}>
-          <Text>My-App4 {i + 1} </Text>
-        </View>
-      ))}
-
-      <Image source={require('./assets/web.webp')} />
-      <Image source={require('./assets/web.webp')} />
-      <Image source={require('./assets/web.webp')} />
 
 
-      <StatusBar style="auto" />
-    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 16,
-    alignItems: 'center'
+    paddingVertical: 40,
+    backgroundColor: '#e65100'
   },
-  item: {
-    width: '90%',
-    padding: 10,
-    backgroundColor: '#a8d2ffff',
-    margin: 5,
-    borderRadius: 10
+  box: {
+    width: 100,
+    height: 100,
+    marginRight: 12,
+    backgroundColor: '#ffe0b2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12
+  },
+  letter: {
+    fontWeight: 800,
+    color: '#e65100'
+  },
+  gap: {
+    marginRight: 10
   }
 });
