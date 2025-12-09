f3865e81c6eff2f7abd2a049acd6d8d081e84ee6
kpamukcu <kapawebtasarim@gmail.com>
2025-12-09 16:54:16 +0300

    my-app8

--- my-app8/App.tsx
+++ my-app8/App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Modal, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [visible, setVisible] = useState(false)
+
+  //onRequestClose={() => setVisible(false)} android geri tuışu için şart
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Modal</Text>
+      <Button title="Modal Aç" onPress={() => setVisible(true)} />
+
+      <Modal animationType='slide' visible={visible} onRequestClose={() => setVisible(false)} transparent>
+        <View style={styles.modal}>
+          <Text>Modal Açıldı</Text>
+          <Button title='Modal Kapat' onPress={() => setVisible(false)} />
+        </View>
+      </Modal>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modal: {
+    flex: 1,
+    backgroundColor: '#ff8e8eff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
